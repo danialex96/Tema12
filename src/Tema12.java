d8d569f12bc6ecdf671ed034cfce46a5b8a6ad7d
Cuc Daniel Alexandru <danialex96@yahoo.com>
2017-11-10 18:44:06 +0200

    Initial commit

---
+++ src/Tema12.java
@@ -0,0 +1,15 @@
+public class Tema12 {
+    public static void main(String[] args) {
+
+        int num = 20, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
+
